f4e5bc80e6c2597770aae12dc19323c6b3d83e40
Incognito-Z0 <90766078+Incognito-Z0@users.noreply.github.com>
2022-11-01 10:48:07 +0100

    Add files via upload

---
+++ HOME-PROJECT/app.js
@@ -0,0 +1,13 @@
+//DOM ELEMENTS
+const ham = document.querySelector('.hamburger-menu');
+const navMenu = document.querySelector('.ul')
+
+//HAMBURGER DOM IMPLEMENTATION
+// ham.addEventListener('click', function(){
+//     navMenu.classList.toggle('open');
+// })
+
+ham.addEventListener('click', function(){
+    navMenu.classList.toggle('open')
+})
+
